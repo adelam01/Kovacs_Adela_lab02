eb86d783d5952aa660c910a66e4f5d81112b5066
Kovacs Adela <adela17.21@icloud.com>
2022-11-05 15:55:47 +0200

    laborator04

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,10 +1,22 @@
-﻿namespace Kovacs_Adela_lab02.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Kovacs_Adela_lab02.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Kovacs_Adela_lab02.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
